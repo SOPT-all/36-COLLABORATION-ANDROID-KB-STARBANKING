a304591d90b77fbab6dc50ba2399431cd093da9a
Kim Nahyeon <nhyeonii@naver.com>
2025-05-22 16:40:31 +0900

    mod/#24: mainActivity 원래대로 수정

--- app/src/main/java/org/sopt/starbanking/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/starbanking/presentation/main/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import org.sopt.starbanking.presentation.savingsDetail.SavingsDetailScreen
 import org.sopt.starbanking.ui.theme.StarBankingTheme
 
 @AndroidEntryPoint
@@ -19,8 +18,7 @@ class MainActivity : ComponentActivity() {
             // TODO : val mainNavigator = rememberMainNavigator(navigator)
 
             StarBankingTheme {
-                SavingsDetailScreen(
-                    accountId = 2
+                MainScreen(
                     // TODO: navigator = mainNavigator
                 )
             }
